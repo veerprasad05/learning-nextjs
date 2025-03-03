ec509641428857b6481a31e73fa7fedba88af62e
veerprasad05 <veerprasad@gmail.com>
2025-03-03 16:59:32 -0500

    added invoices on search

--- nextjs-dashboard/app/ui/search.tsx
+++ nextjs-dashboard/app/ui/search.tsx
@@ -8,12 +8,11 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  const [searchTerm, setSearchTerm] = useState('');
 
-  function handleSearch() {
+  function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
-    if (searchTerm) {
-      params.set('query', searchTerm);
+    if (term) {
+      params.set('query', term);
     } else {
       params.delete('query');
     }
@@ -27,16 +26,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        value={searchTerm}
         onChange={(e) => {
-          setSearchTerm(e.target.value);
+          handleSearch(e.target.value);
         }}
-        onKeyDown={(e) => {
-          if (e.key == 'Enter') {
-            e.preventDefault();
-            handleSearch();
-          }
-        }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
